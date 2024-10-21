8020c134de3cd70e0d3976d31f39388de886274b
Lost Soul <bipin@bi3.co.uk>
2024-10-21 15:32:39 +0100

    New Command structure and base functionality testing

--- src/Commands/LaraVaultCommand.php
+++ src/Commands/LaraVaultCommand.php
@@ -2,9 +2,17 @@
 
 namespace Voidoflimbo\LaraVault\Commands;
 
+use RuntimeException;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\File;
+use Illuminate\Filesystem\Filesystem;
 use Symfony\Component\Process\Process;
+
+use function Laravel\Prompts\{
+    confirm,
+    info,
+    note,
+    error
+};
 
 class LaraVaultCommand extends Command
 {
@@ -14,46 +22,122 @@ class LaraVaultCommand extends Command
 
     public function handle()
     {
-        $this->info('Installing Laravel Breeze...');
+        // Inform the user that Laravel Breeze is being installed
+        info('Installing Laravel Breeze...');
         if (! $this->runProcess(['composer', 'require', 'laravel/breeze', '--dev'])) {
             return 1;
         }
+        info('Laravel Breeze Installed !');
 
-        $this->info('Publishing Breeze scaffolding...');
+        // Inform the user that Breeze scaffolding is being published
+        info('Publishing Breeze scaffolding...');
         if (! $this->runProcess(['php', 'artisan', 'breeze:install', 'blade', '--pest'])) {
             return 1;
         }
+        info('Published Breeze scaffolding !');
 
-        $this->info('Replacing Breeze Blade files with custom files...');
+        // TODO: Complete replacing of all default blade files
+        // Replace Breeze Blade files with custom files
+        info('Replacing Breeze Blade files with custom files...');
         $this->replaceFiles(
             resource_path('views/auth'),
             $this->packageResourcePath('views/auth')
         );
+        info('Breeze Blade files replaced with custom files !');
 
-        $this->info('Installing Livewire...');
+        // Inform the user that Livewire is being installed
+        info('Installing Livewire...');
         if (! $this->runProcess(['composer', 'require', 'livewire/livewire'])) {
             return 1;
         }
+        info('Livewire Installed !');
 
-        $this->info('Replacing app.js file...');
-        $this->replaceFile(
+        // Replace the app.js file with a custom file
+        info('Replacing app.js file...');
+        $this->replaceFiles(
             resource_path('js/app.js'),
             $this->packageResourcePath('js/app.js')
         );
+        info('app.js Replaced !');
 
-        $this->info('LaraVault installation complete.');
-        //already ran by breeze
-        // $this->info('Please run the following commands to complete the setup:');
-        // $this->info('- npm install');
-        // $this->info('- npm run dev');
-        $this->info('Also please copy these images in public/img');
-        $this->info('- logo.png');
-        $this->info('- logo.ico');
-        $this->info('- login_bg.png');
+        // Replace the public directory
+        info('Replacing app.js file...');
+        $this->replaceFiles(
+            public_path('img/logo.ico'),
+            $this->packagePublicPath('img/logo.ico')
+        );
+        $this->replaceFiles(
+            public_path('img/logo.png'),
+            $this->packagePublicPath('img/logo.png')
+        );
+        $this->replaceFiles(
+            public_path('img/login_bg.png'),
+            $this->packagePublicPath('img/login_bg.png')
+        );
+        info('app.js Replaced !');
 
+        // Copy over the Blade anonymous components
+        info('Copying anonymous blade components');
+        $this->replaceFiles(
+            resource_path('views/components'),
+            $this->packageResourcePath('views/components')
+        );
+        info('Anonymous blade components copied');
+
+        // Output a blank line for readability
+        $this->line('');
+
+        // Inform the user that the installation is complete
+        info('LaraVault installation complete.');
+        $this->line('');
+
+        // Ask the user if they want to run npm install and npm run build
+        if (confirm(
+            label: 'Do you want to run npm install && npm run build',
+            default: true,
+            yes: 'Yes run now',
+            no: 'No I will install it later',
+            hint: 'Install later only if specific use case demands it'
+        )) {
+            $this->runCommands(['npm install', 'npm run build']);
+            $this->line('');
+        }
+
+        // Display a note about replacing images in the public/img directory
+        note('You should probably replace followings in public/img \n - logo.png\n - logo.ico\n - login_bg.png');
         return 0;
     }
 
+    /**
+     * Run the given commands.
+     *
+     * @param  array  $commands
+     * @return void
+     */
+    protected function runCommands(array $commands)
+    {
+        $process = Process::fromShellCommandline(implode(' && ', $commands), null, null, null, null);
+
+        if ('\\' !== DIRECTORY_SEPARATOR && file_exists('/dev/tty') && is_readable('/dev/tty')) {
+            try {
+                $process->setTty(true);
+            } catch (RuntimeException $e) {
+                $this->output->writeln('  <bg=yellow;fg=black> WARN </> ' . $e->getMessage() . PHP_EOL);
+            }
+        }
+
+        $process->run(function ($type, $line) {
+            $this->output->write('    ' . $line);
+        });
+    }
+
+    /**
+     * Run a process with the given command.
+     *
+     * @param  array  $command
+     * @param  int  $time
+     * @return bool
+     */
     protected function runProcess(array $command, int $time = 300)
     {
         $process = new Process($command);
@@ -61,14 +145,14 @@ class LaraVaultCommand extends Command
         $process->setTty(Process::isTtySupported());
         $process->run(function ($type, $buffer) {
             if ($type === Process::ERR) {
-                $this->error($buffer);
+                error($buffer);
             } else {
-                $this->info($buffer);
+                info($buffer);
             }
         });
 
         if (! $process->isSuccessful()) {
-            $this->error('Failed to execute: '.implode(' ', $command));
+            error('Failed to execute: ' . implode(' ', $command));
 
             return false;
         }
@@ -76,26 +160,51 @@ class LaraVaultCommand extends Command
         return true;
     }
 
+    /**
+     * Replace files or directories in the target path with those from the source path.
+     *
+     * @param  string  $targetPath
+     * @param  string  $sourcePath
+     * @return void
+     */
     protected function replaceFiles($targetPath, $sourcePath)
     {
-        if (File::exists($targetPath)) {
-            File::deleteDirectory($targetPath);
+        $filesystem = new Filesystem();
+
+        if ($filesystem->exists($targetPath)) {
+            if ($filesystem->isDirectory($targetPath)) {
+                $filesystem->deleteDirectory($targetPath);
+            } else {
+                $filesystem->delete($targetPath);
+            }
         }
 
-        File::copyDirectory($sourcePath, $targetPath);
-    }
-
-    protected function replaceFile($targetFile, $sourceFile)
-    {
-        if (File::exists($targetFile)) {
-            File::delete($targetFile);
+        if ($filesystem->isDirectory($sourcePath)) {
+            $filesystem->copyDirectory($sourcePath, $targetPath);
+        } else {
+            $filesystem->copy($sourcePath, $targetPath);
         }
-
-        File::copy($sourceFile, $targetFile);
     }
 
+    /**
+     * Get the package resource path.
+     *
+     * @param  string  $path
+     * @return string
+     */
     protected function packageResourcePath($path)
     {
-        return __DIR__.'/../../resources/'.$path;
+        return __DIR__ . '/../../resources/' . $path;
+    }
+
+    /**
+     * Get the package public path.
+     *
+     * @param  string  $path
+     * @return string
+     */
+    protected function packagePublicPath($path)
+    {
+        return __DIR__ . '/../../public/' . $path;
     }
 }
--- src/LaraVaultServiceProvider.php
+++ src/LaraVaultServiceProvider.php
@@ -14,18 +14,6 @@ class LaraVaultServiceProvider extends PackageServiceProvider
         $package
             ->name('laravault')
             ->hasConfigFile()
-            ->hasCommand(LaraVaultCommand::class)
-            ->hasViews();
-    }
-
-    public function boot()
-    {
-        parent::boot();
-
-        // Register the views
-        // $this->loadViewsFrom(__DIR__ . '/../../resources/views/voidoflimbo', 'voidoflimbo');
-
-        // Register anonymous Blade components
-        Blade::anonymousComponentPath(__DIR__.'/../../resources/views/voidoflimbo', 'voidoflimbo');
+            ->hasCommand(LaraVaultCommand::class);
     }
 }
